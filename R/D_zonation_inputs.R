1960e4404f355c9f26b575002c18cdb85a6660c0
João Paulo Alencar <joaosvalencar@Joaos-MacBook-Pro-2.local>
2025-06-14 23:14:58 -0300

    new spp rasters

--- R/D_zonation_inputs.R
+++ R/D_zonation_inputs.R
@@ -4,15 +4,19 @@
 zon_ex <- read.table(here::here("data","zonation_example", "feature_list_DD.txt"), header = TRUE)
 head(zon_ex)
 
-lost_path <- "/Users/joaosvalencar/Downloads/lost_rasters"
+path_lost <- "/Users/joaosvalencar/Documents/priorityRediscovery/zonation/rasters/lost_rasters/"
 
 # Lost species archive ----------------------------------------------------
 lost_filename <- c(dir(lost_path)) #lost filenames
-
 filename <- paste0("../lost_rasters/", lost_filename)
+clean_files <- gsub(".*/[^_]+_([^_]+)_([^\\.]+)\\.tif$", "\\1 \\2", filename)
 
-lost_names <- sub("^[^_]+_(.+)\\.tif$", "\\1", lost_filename)
-toMerge <- data.frame(lost_names, filename)
+lost_names <- lost_spp$species
+
+matched_idx <- match(lost_names, clean_files) #creates vector of ids where lost_names and clean_files match
+matched_files <- filename[matched_idx] #orders filename according to matching ids
+
+toMerge <- data.frame(lost_names, matched_files) #merge species names with file names
 
 lost_stats <- spp_stats[spp_stats$source=="lost_spp",]
 
@@ -24,43 +28,50 @@ head(lost_data)
 lost_data <- lost_data[,c(1,3)]
 
 feature_list_lost <- merge(feature_list_lost, lost_data, by="id_no")
-
+head(feature_list_lost)
 
 
 #has to add five columns: condition, group, weight, wgrp, filename
-feature_list_lost <- data.frame(condition = rep(NA, times = dim(lost_data)[1]),
+feature_list_lost_file <- data.frame(
                                 group = rep(NA, times = dim(lost_data)[1]),
                                 weight = rep(NA, times = dim(lost_data)[1]),
                                 wgrp = rep(NA, times = dim(lost_data)[1]),
                                 filename = rep(NA, times = dim(lost_data)[1]))
 
-head(feature_list_lost)
+head(feature_list_lost_file)
 
-feature_list_lost$condition <- rep(1, times = dim(lost_data)[1])
-feature_list_lost$group <- match(feature_list_DD$class, c("Amphibia", "Reptilia", "Aves", "Mammalia"))
-feature_list_lost$weight <- round(feature_list_DD$median_gHM, digits=3) #median
-feature_list_lost$wgrp <- match(feature_list_DD$class, c("Amphibia", "Reptilia", "Aves", "Mammalia"))
-feature_list_lost$filename <- feature_list_lost$filename
+feature_list_lost_file$group <- match(feature_list_lost$class, c("Amphibia", "Reptilia", "Aves", "Mammalia"))
+feature_list_lost_file$weight <- round(feature_list_lost$median_gHM, digits=3) #median
+feature_list_lost_file$wgrp <- match(feature_list_lost$class, c("Amphibia", "Reptilia", "Aves", "Mammalia"))
+feature_list_lost_file$filename <- feature_list_lost$matched_files
 
-head(feature_list_lost)
+head(feature_list_lost_file)
 
-write.table(feature_list_lost, here::here("data", "zonation_rediscovery", "feature_list_lost.txt"),
+write.table(feature_list_lost, here::here("data", "zonation", "feature_list_lost.txt"),
             row.names = FALSE, quote = FALSE)
 
 # DD species archive ------------------------------------------------------
-dd_path <- "/Users/joaosvalencar/Downloads/dd_rasters"
+path_dd <- "/Users/joaosvalencar/Documents/priorityRediscovery/zonation/rasters/dd_rasters/"
 
-DD_filename <- c(dir(dd_path)) #DD filenames
+DD_filename <- c(dir(path_dd)) #DD filenames
 
 filename <- paste0("../dd_rasters/", DD_filename)
 
-dd_names <- sub("^[^_]+_(.+)\\.tif$", "\\1", DD_filename)
-toMergeDD <- data.frame(dd_names, filename)
+clean_files <- gsub(".*/[^_]+_([^_]+)_([^\\.]+)\\.tif$", "\\1 \\2", filename)
+
+dd_names <- dd_poly$species
+
+matched_idx <- match(dd_names, clean_files) #creates vector of ids where lost_names and clean_files match
+matched_files <- filename[matched_idx] #orders filename according to matching ids
+
+toMergeDD <- data.frame(dd_names, matched_files) #merge species names with file names
+head(toMerge)
 
 dd_stats <- spp_stats[spp_stats$source=="dd_spp",]
 
 feature_list_DD <- merge(dd_stats, toMergeDD, by.x="species", by.y="dd_names")
 feature_list_DD <- feature_list_DD[,-5]
+head(feature_list_DD)
 
 dd_data <- data.frame(dd_poly)
 head(dd_data)
@@ -70,21 +81,20 @@ feature_list_DD <- merge(feature_list_DD, dd_data, by="id_no")
 head(feature_list_DD)
 
 
-feature_list_DD_df <- data.frame(condition = rep(NA, times = dim(dd_data)[1]),
+feature_list_DD_file <- data.frame(
                                 group = rep(NA, times = dim(dd_data)[1]),
                                 weight = rep(NA, times = dim(dd_data)[1]),
                                 wgrp = rep(NA, times = dim(dd_data)[1]),
                                 filename = rep(NA, times = dim(dd_data)[1]))
 
-head(feature_list_DD_df)
+head(feature_list_DD_file)
 
-feature_list_DD_df$condition <- rep(1, times = dim(dd_data)[1])
-feature_list_DD_df$group <- match(feature_list_DD$class, c("Amphibia", "Reptilia", "Aves", "Mammalia"))
-feature_list_DD_df$weight <- round(feature_list_DD$median_gHM, digits=3) #median
-feature_list_DD_df$wgrp <- match(feature_list_DD$class, c("Amphibia", "Reptilia", "Aves", "Mammalia"))
-feature_list_DD_df$filename <- feature_list_DD$filename
+feature_list_DD_file$group <- match(feature_list_DD$class, c("Amphibia", "Reptilia", "Aves", "Mammalia"))
+feature_list_DD_file$weight <- round(feature_list_DD$median_gHM, digits=3) #median
+feature_list_DD_file$wgrp <- match(feature_list_DD$class, c("Amphibia", "Reptilia", "Aves", "Mammalia"))
+feature_list_DD_file$filename <- feature_list_DD$matched_files
 
-head(feature_list_DD_df)
+head(feature_list_DD_file)
 
-write.table(feature_list_DD_df, here::here("data", "zonation_rediscovery", "feature_list_DD.txt"),
+write.table(feature_list_DD_file, here::here("data", "zonation", "feature_list_DD.txt"),
             row.names = FALSE, quote = FALSE)
--- data/zonation_rediscovery/EDIT_z5_calls_wrg&wcrg.cmd
+++
@@ -1,5 +0,0 @@
-@setlocal
-@PATH=C:\Program Files\Zonation5;%PATH%
-
-z5 -wrg --mode=ABF --gui settings_wcrg.z5 out_wrg
-z5 -wcrg --mode=ABF --gui settings_wcrg.z5 out_wcrg
